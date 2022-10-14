9e0bb99d6c31a88eb21b8dfa9a0b4e41149cccee
Marcos Aurelio <maurelio@pop-os.localdomain>
2022-10-13 23:50:20 -0300

    Criação do esqueleto da página

---
+++ resources/views/inicial.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu portifólio</title>
+</head>
+<body>
+    <h1>Marquim aqui, friends</h1>
+    <h2>Quem sou eu??</h2>
+    <img src={{url("/storage/avatar.jpg")}} alt="Error">
+    <p>Olá mundo, sou o Marcos. Sou um estudante de Análise e Desenvolvimento de Sistemas que, no auge da vontade de aprender, resolveu criar uma página de perfil para sua própria pessoa. Espero que sua experiência na página seja a melhor possível</p>
+    <h2>Como falar comigo?</h2>
+    <ul>
+        <li>
+            <a href="https://github.com/MarcosAur" target=_blank>
+            <img src="/storage/icone-github.png" alt="error">
+            /MarcosAur
+            </a> - Acessa meu github para ver mais projetos
+        </li>
+        <li>
+            <a href="https://www.instagram.com/marcos.aurelio.m/" target=_blank>
+            <img src="/storage/icone-instagram.png" alt="error">
+            /marcos.aurelio.m
+            </a> - Acessa meu instagram para me ver
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/marcos-aur%C3%A9lio-5862a3160/" target=_blank>
+            <img src="/storage/icone-linkedin.png" alt="error">
+            /marcos-aurelio
+            </a> - Me segue no linkdin  
+        </li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicial');
+});
+
+Route::get('/imagem', function () {
+    return url('perfil.png');
 });
